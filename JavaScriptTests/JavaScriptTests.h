3fd792d2948db3316fc2f3a5f862ed2c65a959ee
Anselz <anselobjective@gmail.com>
2012-11-27 20:42:57 +0800

    Initial Commit

---
+++ JavaScriptTests/JavaScriptTests.h
@@ -0,0 +1,13 @@
+//
+//  JavaScriptTests.h
+//  JavaScriptTests
+//
+//  Created by Ansel on 12-11-27.
+//  Copyright (c) 2012年 Ansel. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface JavaScriptTests : SenTestCase
+
+@end
---
+++ JavaScriptTests/JavaScriptTests.m
@@ -0,0 +1,32 @@
+//
+//  JavaScriptTests.m
+//  JavaScriptTests
+//
+//  Created by Ansel on 12-11-27.
+//  Copyright (c) 2012年 Ansel. All rights reserved.
+//
+
+#import "JavaScriptTests.h"
+
+@implementation JavaScriptTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in JavaScriptTests");
+}
+
+@end
